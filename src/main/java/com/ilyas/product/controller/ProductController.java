061f78c5743da475ebe4f8458784b2054310d018
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-05-27 00:00:04 +0800

    add cross origin annotation

--- src/main/java/com/ilyas/product/controller/ProductController.java
+++ src/main/java/com/ilyas/product/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,6 +34,7 @@ public class ProductController {
     @Autowired
     RestTemplate restTemplate;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<Slice<Product>> getAllProducts(
         @RequestParam(defaultValue = "0") Integer pageNo, 
